404c55820a34f81afa8669e4d767abd6d91bce10
Verity Collins <verity.carlos@gmail.com>
2022-10-10 14:28:41 -0500

    added tables to readers and books migration files

--- db/migrate/20221010191852_create_books.rb
+++ db/migrate/20221010191852_create_books.rb
@@ -1,4 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :level
+      t.integer :reader_id
+    end 
   end
 end
